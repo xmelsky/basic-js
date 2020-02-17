faafa1bf7e45955728bfd4b1c775ca9eb415a324
dima xmiel <dima.hmelios@gmail.com>
2020-02-17 19:41:32 +0100

    feat: solve tower of hanoi task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,4 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = 2 ** disksNumber - 1;
+  return { turns, seconds: turns / (turnsSpeed / 3600) };
+};
