66540dd5bf61deb549ffd3f6ebde8ab03b11cbdc
dima xmiel <dima.hmelios@gmail.com>
2020-02-17 14:55:35 +0100

    feat: solve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const K = 0.693 / HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(value) {
+  value = typeof value === "string" && parseFloat(value);
+  return (
+    !isNaN(value) &&
+    value > 0 &&
+    value < 15 &&
+    Math.ceil(Math.log(MODERN_ACTIVITY / value) / K)
+  );
 };
