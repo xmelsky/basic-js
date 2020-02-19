a9f2d63607226fa854b669566d0e1fb007bead25
dima xmiel <dima.hmelios@gmail.com>
2020-02-19 12:26:32 +0100

    feat: solve extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,23 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(
+  str,
+  {
+    repeatTimes = 1,
+    separator = "+",
+    addition,
+    additionRepeatTimes = 0,
+    additionSeparator = "|"
+  } = {}
+) {
+  str = str && str.toString();
+  addition = addition && addition.toString();
+  let result = "";
+  for (let i = repeatTimes; i > 0; i--) {
+    result += str;
+    for (let y = additionRepeatTimes; y > 0; y--) {
+      result += addition + (y - 1 > 0 ? additionSeparator : "");
+    }
+    result += i - 1 > 0 ? separator : "";
+  }
+  result += !additionRepeatTimes && addition ? addition : "";
+  return result;
 };
-  
