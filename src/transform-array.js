92eb79ee12f3577622797b7aa8d9835bf84bac6d
dima xmiel <dima.hmelios@gmail.com>
2020-02-19 11:11:00 +0100

    feat: solve transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error();
+  const result = [];
+  const controls = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev"
+  ];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (controls.includes(arr[i])) {
+      switch (arr[i]) {
+        case "--discard-next":
+          if (i + 2 < arr.length) i++;
+          break;
+        case "--discard-prev":
+          result.pop();
+          break;
+        case "--double-next":
+          if (i + 1 < arr.length) result.push(arr[i + 1]);
+          break;
+        default:
+          if (i - 1 >= 0) result.push(arr[i - 1]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
